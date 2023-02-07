5e87830069b01c564b179c08ea2c12d63a552a32
t0305scvs <105956752+t0305scvs@users.noreply.github.com>
2023-02-07 22:05:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -25,8 +25,9 @@ for (i = 1; i < 25; i++) {
 
 document.getElementById('return').onclick = function () {
     // alert('還沒有辦法送出QQ');
+    event.preventDefault();
     html2canvas(document.getElementById('container')).then(function (canvas) {
-        var pic = document.createElement('a');
+        let pic = document.createElement('a');
         pic.href = canvas.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
         pic.download = 'record.jpg';
         pic.click();
